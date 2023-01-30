adf0de8652af0ba472ed3e50de53635de1a7c388
TomSnowflake <123960106+TomSnowflake@users.noreply.github.com>
2023-01-30 16:04:06 +0100

    Update streamlit_app.py
    typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 
 streamlit.title('My Partents New Healthy Diner')
 
-streamlit.hesder('Breakfast menu')
+streamlit.header('Breakfast menu')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
