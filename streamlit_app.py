9810ffb81adafc02fe4e152cdc954b0394cc6588
TomSnowflake <123960106+TomSnowflake@users.noreply.github.com>
2023-02-01 14:22:47 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,18 +25,22 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  if not fruit_choice:
+      streamlit.error("Please select a fruit to get information.")
+   else:
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+        fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+        streamlit.dataframe(fruityvice_normalized)
 
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
+        
+   except URLError as e:
+      streamlit.error()
+
+ streamlit.write('The user entered ', fruit_choice)
 
 
-#import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-
-# layout verbeteren van json 
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# zet de mooie layout neer
-streamlit.dataframe(fruityvice_normalized)
 
 streamlit.stop()
 
